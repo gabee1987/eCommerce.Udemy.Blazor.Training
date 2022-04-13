fd3155df5e390cf274a4cd25e47bbe2fbdbbb207
Gabor Koncz <gabee1987@gmail.com>
2022-04-13 16:41:23 +0200

    finished the data binding example with added design and bind types

--- eCommerce.Blazor.Web.Models/Models/DemoProduct.cs
+++ eCommerce.Blazor.Web.Models/Models/DemoProduct.cs
@@ -11,5 +11,6 @@ namespace eCommerce.Blazor.Web.Models.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
+        public double Price { get; set; }
     }
 }
