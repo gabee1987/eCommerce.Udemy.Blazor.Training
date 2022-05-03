66a511da19baced286b764f2c401943df8b7b275
Gabor Koncz <gabee1987@gmail.com>
2022-05-03 15:46:57 +0200

    created a shared component for the demo product cards

---
+++ eCommerce.Blazor.Web.Server/Components/_DemoProductComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using eCommerce.Blazor.Web.Models.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace eCommerce.Blazor.Web.Server.Components
+{
+    public partial class _DemoProductComponent
+    {
+        [Parameter]
+        public DemoProduct Product { get; set; }
+    }
+}
