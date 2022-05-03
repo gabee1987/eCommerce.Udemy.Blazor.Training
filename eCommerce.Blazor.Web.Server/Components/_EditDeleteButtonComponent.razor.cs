2ebff8d144245956bf23d0a5d617769b9d6c89d0
Gabor Koncz <gabee1987@gmail.com>
2022-05-03 15:37:54 +0200

    defined a parameter for the shared component, to be able to show and hide it

---
+++ eCommerce.Blazor.Web.Server/Components/_EditDeleteButtonComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eCommerce.Blazor.Web.Server.Components
+{
+    public partial class _EditDeleteButtonComponent
+    {
+        [Parameter]
+        public bool ProductIsActive { get; set; }
+    }
+}
